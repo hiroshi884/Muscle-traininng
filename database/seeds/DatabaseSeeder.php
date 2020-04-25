ab082ca2e255f552d8743c3b869f7cee7240910e
hiroshi884 <hiro8843313@icloud.com>
2020-04-25 19:06:18 +0900

    Laravel入門チュートリアルです

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(BooksTableSeeder::class);
+        // BooksTableSeederを読み込むように指定
+        $this->call(BooksTableSeeder::class);
     }
 }
