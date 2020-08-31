49cf017cab662357522a1963e292429d1ba20306
hiroshi884 <hiro8843313@icloud.com>
2020-09-01 06:54:44 +0900

    画像挿入

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,16 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="file" class="col-md-4 col-form-label text-md-right">{{ __('画像') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="file" type="file" class="form-control"  name="file"  required autocomplete="file" value="{{ old('file') }}" >
+
+                            </div>
+                        </div>
+
+                            
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
                             <div class="col-md-6">
